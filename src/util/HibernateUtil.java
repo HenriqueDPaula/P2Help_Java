64a4c38f3e985cf0bc98a0eb52ce2e2c65dbe1bf
opet <opet@Academico.edu>
2018-05-02 20:29:47 -0300

    hibernate and Jars file

---
+++ src/util/HibernateUtil.java
@@ -0,0 +1,65 @@
+/**
+ * 
+ */
+package br.edu.uniopet.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+
+/**
+ * Classe para realizar a ponte entre o arquivo de conexão e o banco de dados.
+ * 
+ * @author Baracho
+ * 
+ * @since 26/05/2013
+ * 
+ * @version 1.0
+ * 
+ */
+
+public class HibernateUtil {
+
+	private static SessionFactory sessionFactory = buildSessionFactory();
+
+	private static SessionFactory buildSessionFactory() {
+
+		ServiceRegistry serviceRegistry = null;
+
+		try {
+
+			Configuration configuration = new Configuration();
+
+			configuration.configure("hibernate.cfg.xml");
+
+			serviceRegistry = new ServiceRegistryBuilder().applySettings(
+					configuration.getProperties()).buildServiceRegistry();
+
+			return configuration.buildSessionFactory(serviceRegistry);
+
+		} catch (Throwable e) {
+			// TODO: handle exception
+
+			System.err
+					.println("Criação inicial do objeto SessionFactory falhou. Erro: "
+							+ e.getMessage());
+
+			throw new ExceptionInInitializerError();
+		}
+	}
+
+	public static SessionFactory getSessionFactory() {
+
+		return sessionFactory;
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		HibernateUtil.getSessionFactory();
+	}
+
+}
